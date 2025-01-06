c9a5f99b7c0955220dd98d7eef8234bb38d2c37f
Vuko Vukasinovic <vukasinovicvuko5@gmail.com>
2025-01-06 20:37:25 +0100

    count words

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+def count_words(text):
+    count = 0
+    words = text.split()
+    for word in words:
+        count += 1
+    print(count)
+
 def main():
     with open('./books/frankenstein.txt') as f:
         file_contents = f.read()
-        print(file_contents)
+        count_words(file_contents)
 
 main()
