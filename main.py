a174d83aee514301238acedafee9c09e7eff7100
Vuko Vukasinovic <vukasinovicvuko5@gmail.com>
2025-01-06 21:39:23 +0100

    counting letters in text

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
+def sort_on(entry):
+    return entry["num"]
+
+def count_characters(text):
+    characters = {}
+    for character in text:
+        if character.isalpha():
+            if character in characters:
+                characters[character] += 1
+            else:
+                characters[character] = 1
+    return characters
+
 def count_words(text):
-    count = 0
     words = text.split()
-    for word in words:
-        count += 1
-    print(count)
+    return len(words)
 
 def main():
     with open('./books/frankenstein.txt') as f:
+        print("--- Begin report of books/frankenstein.txt ---")
+
         file_contents = f.read()
-        count_words(file_contents)
+        words_in_document = count_words(file_contents)
+        print(f"{words_in_document} words found in the document\n")
+
+        character_list = []
+        characters_in_document = count_characters(file_contents)
+        for char, count in characters_in_document.items():
+            character_list.append({"char": char, "num": count})
+
+        character_list.sort(reverse=True, key=sort_on)
+
+        for entry in character_list:
+            print(f"The '{entry['char']}' character was found {entry['num']} times")
+
+        print("--- End report ---")
 
 main()
+
